4e395ab4b4142a992ac7f64e6efaa81df80b0e95
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-08-04 15:56:39 +0700

    Up

--- James/_1_Spring MVC/SpringGreeting/src/main/java/com/codegym/controller/GreetingController.java
+++ James/_1_Spring MVC/SpringGreeting/src/main/java/com/codegym/controller/GreetingController.java
@@ -3,15 +3,21 @@ package com.codegym.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
 
-    public String greeting( String name, Model model) {
-        name="ázexdcfvgbhnjmk";
-        model.addAttribute("name", name);
-        return "views";
+//    public String greeting( String name, Model model) {
+//        name="ázexdcfvgbhnjmk";
+//        model.addAttribute("name", name);
+//        return "views";
+@ResponseBody
+    public String greeting(@RequestParam String message) {
+        return message;
     }
 }
