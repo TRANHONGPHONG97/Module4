da90a67e169de1e3df12267fce22e2acefcb832c
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-08-07 20:11:06 +0700

    Up

---
+++ James/_3_ Views_Thymeleaf/product_management_thymeleaf/src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,77 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.repository.IProductRepository;
+import com.codegym.repository.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    private final IProductRepository iProductRepository = new ProductRepository();
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> productList = iProductRepository.findAll();
+        model.addAttribute("product", new Product());
+        model.addAttribute("productList",productList);
+        return "/indexProduct";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("product", new Product());
+        return "/createProduct";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product) {
+        iProductRepository.save(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("product", iProductRepository.findById(id));
+        return "/editProduct";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        iProductRepository.update(product.getId(), product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("product", iProductRepository.findById(id));
+        return "/deleteProduct";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Product product, RedirectAttributes redirect) {
+        iProductRepository.remove(product.getId());
+        redirect.addFlashAttribute("success", "Removed product successfully!");
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("product", iProductRepository.findById(id));
+        return "/viewProduct";
+    }
+
+    @GetMapping("/search")
+    public String search(Product product, Model model) {
+        model.addAttribute("productList", iProductRepository.searchByName(product));
+        return "/indexProduct";
+    }
+}
---
+++ James/_3_ Views_Thymeleaf/product_management_thymeleaf/src/main/java/com/codegym/repository/ProductRepository.java
@@ -0,0 +1,70 @@
+package com.codegym.repository;
+
+import com.codegym.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository implements IProductRepository {
+    static List<Product> productList = new ArrayList<>();
+
+    static {
+        productList.add(new Product(1, "G102", "300", "100%", "Logitech"));
+        productList.add(new Product(2, "G304", "600", "100%", "Logitech"));
+        productList.add(new Product(3, "G502", "1200", "100%", "Logitech"));
+        productList.add(new Product(4, "G402", "800", "100%", "Logitech"));
+        productList.add(new Product(5, "G903", "1500", "100%", "Logitech"));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productList;
+    }
+
+    @Override
+    public void save(Product product) {
+        productList.add(product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        for (int i = 0; i < productList.size(); i++) {
+            if (id == productList.get(i).getId()) {
+                return productList.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        for (int i = 0; i < productList.size(); i++) {
+            if (id == productList.get(i).getId()) {
+                productList.get(i).setName(product.getName());
+                productList.get(i).setPrice(product.getPrice());
+                productList.get(i).setDetail(product.getDetail());
+                productList.get(i).setManufacturer(product.getManufacturer());
+            }
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        for (int i = 0; i < productList.size(); i++) {
+            if (id == productList.get(i).getId()) {
+                productList.remove(i);
+            }
+        }
+    }
+
+    @Override
+    public List<Product> searchByName(Product product) {
+        List<Product> listSearchByName = new ArrayList<>();
+        for (int i = 0; i < productList.size(); i++) {
+            if (productList.get(i).getName().contains(product.getName())) {
+                listSearchByName.add(productList.get(i));
+            }
+        }
+        return listSearchByName;
+    }
+}
