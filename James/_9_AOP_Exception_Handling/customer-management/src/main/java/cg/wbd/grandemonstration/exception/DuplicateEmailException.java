261a526a418ae869ba1061c3c43ce671249cbe0a
Trần Hồng Phong <103017248+TRANHONGPHONG97@users.noreply.github.com>
2022-08-23 11:44:28 +0700

    Delete James/_9_AOP_Exception_Handling/customer-management directory

--- James/_9_AOP_Exception_Handling/customer-management/src/main/java/cg/wbd/grandemonstration/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package cg.wbd.grandemonstration.exception;
-
-import cg.wbd.grandemonstration.model.Customer;
-import org.springframework.dao.DataIntegrityViolationException;
-
-public class DuplicateEmailException extends Exception{
-
-}
