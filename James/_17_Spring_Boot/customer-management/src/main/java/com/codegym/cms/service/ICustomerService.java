df8d5dd6a927e481797f956d247d61e746f6c7c3
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-08-26 10:10:13 +0700

    Up

---
+++ James/_17_Spring_Boot/customer-management/src/main/java/com/codegym/cms/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.codegym.cms.service;
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
